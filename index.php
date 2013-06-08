608cf1aef60cc12c3357ce112466bdd56bdcc038
Michal Szymczak <pro@maikeru.pl>
2013-06-08 15:56:35 +0200

    2 commit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
 require 'vendor/autoload.php';
-echo "works";
+
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
+
+
+$foo = new Acme\Foo();
+
+echo "works2";
